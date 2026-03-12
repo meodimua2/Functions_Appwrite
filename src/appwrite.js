b6f7ce90ce845e00b732637c5b1aa4a64f5f3c0e
tieunhichomuoicanruou <tieunhichomuoicanruou@gmail.com>
2026-03-12 11:55:11 +0700

    v8

--- src/appwrite.js
+++ src/appwrite.js
@@ -2,7 +2,7 @@ const { Client, Databases } = require("node-appwrite");
 
 const client = new Client()
     .setEndpoint(process.env.APPWRITE_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setProject(process.env.APPWRITE_PROJECT_ID)
     .setKey(process.env.APPWRITE_API_KEY);
 
 const databases = new Databases(client);
