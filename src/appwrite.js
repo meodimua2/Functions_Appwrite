a0f04b92e0b9413b3ed10927002b98c78010e98c
VoChiBinhThienHa <tieunhichomuoicanruou@gmail.com>
2026-03-12 12:29:28 +0700

    Change Appwrite project ID to a specific value
    Updated project ID in Appwrite client configuration.

--- src/appwrite.js
+++ src/appwrite.js
@@ -2,7 +2,7 @@ const { Client, Databases } = require("node-appwrite");
 
 const client = new Client()
     .setEndpoint(process.env.APPWRITE_ENDPOINT)
-    .setProject(process.env.APPWRITE_PROJECT_ID)
+    .setProject("69ae327900231c1fca3c")
     .setKey(process.env.APPWRITE_API_KEY);
 
 const databases = new Databases(client);
