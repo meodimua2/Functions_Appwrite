bcc6a71588066ffbada7772818396d9f5312f697
VoChiBinhThienHa <tieunhichomuoicanruou@gmail.com>
2026-03-19 21:01:01 +0700

    Refactor DatabaseService for user handling

--- src/telegram-data.js
+++ src/telegram-data.js
@@ -1,86 +1,35 @@
 const { databases } = require("./appwrite");
 
 class DatabaseService {
-    
     constructor() {
-        this.dbId = "69b22e5d00106ba0308f";
-        this.collectionId = "daohuulo";
-    }
-
-    sanitizeTelegramUser(tgUser) {
-        return {
-            telegramId: tgUser.id.toString(),
-            username: String(tgUser.username || "").slice(0, 64),
-            firstName: String(tgUser.first_name || "").slice(0, 64),
-            lastName: String(tgUser.last_name || "").slice(0, 64),
-            photoUrl: String(tgUser.photo_url || "")
-        };
-    }
-
-    sanitizeResponse(user) {
-        return {
-            telegramId: user.telegramId,
-            username: user.username,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            photoUrl: user.photoUrl,
-
-            balance: user.balance,
-            exp: user.exp,
-
-            streak: user.streak,
-            last_checkin: user.last_checkin,
-
-            onboarded: user.onboarded
-        };
+        this.dbId = "69b22e5d00106ba0308f"; 
+        this.collectionId = "users";
     }
 
     async getOrCreateUser(tgUser) {
-
-        const safeUser = this.sanitizeTelegramUser(tgUser);
-        const telegramId = safeUser.telegramId;
+        const telegramId = String(tgUser.id);
 
         try {
+            const user = await databases.getDocument(
+                this.dbId,
+                this.collectionId,
+                telegramId
+            );
 
-            const user = await databases.getDocument({
-                databaseId: this.dbId,
-                collectionId: this.collectionId,
-                documentId: telegramId
-            });
-
-            return {
-                user: this.sanitizeResponse(user),
-                isNewUser: false
-            };
+            return { userId: user.$id };
 
         } catch (err) {
             if (err.code === 404) {
-                const newUser = await databases.createDocument({
-                    databaseId: this.dbId,
-                    collectionId: this.collectionId,
-                    documentId: telegramId,
-                    data: {
-                        ...safeUser,
+                const newUser = await databases.createDocument(
+                    this.dbId,
+                    this.collectionId,
+                    telegramId,      
+                );
 
-                        balance: 0,
-                        exp: 0,
-
-                        streak: 0,
-                        last_checkin: null,
-
-                        onboarded: false,
-
-                        $createdAt: new Date().toISOString()
-                    }
-                });
-
-                return {
-                    user: this.sanitizeResponse(newUser),
-                    isNewUser: true
-                };
+                return { userId: newUser.$id };
             }
 
-            console.error("DatabaseService error:", err.message);
+            console.error("Database Error:", err.message);
             throw err;
         }
     }
