c785f4da3c0269ede7fc58973f0e70e12d164c68
Mèo đi Mưa <tieunhichomuoicanruou@gmail.com>
2026-04-14 21:39:59 +0700

    Refactor getOrCreateUser and update user formatting

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -8,37 +8,71 @@ class UserService {
 
     async getOrCreateUser({ id }) {
         const stringId = String(id);
-
+    
         try {
             const user = await databases.getDocument(
                 this.dbId,
                 this.usersCol,
                 stringId
             );
-
             return this._formatUser(user);
-
+    
         } catch (err) {
-            if (err.code === 404) {
-                const newUser = await databases.createDocument(
-                    this.dbId,
-                    this.usersCol,
-                    stringId, 
-                    {
-                        telegramId: stringId
-                    }
-                );
-
-                return this._formatUser(newUser);
+            if (err.code === 404 || err.type === "document_not_found") {
+                try {
+                    const newUser = await databases.createDocument(
+                        this.dbId,
+                        this.usersCol,
+                        stringId,
+                        {
+                            puuid: null,
+                            gameName: null,
+                            tagLine: null,
+                            tier: null,
+                            rank: null,
+                            leaguePoints: 0,
+                            profileIconId: null
+                        }
+                    );
+                    return this._formatUser(newUser);
+                } catch (createErr) {
+                    const user = await databases.getDocument(
+                        this.dbId,
+                        this.usersCol,
+                        stringId
+                    );
+                    return this._formatUser(user);
+                }
             }
             throw err;
         }
     }
+    
+    async updateTftInfo(telegramId, tftData = {}) {
+        return await databases.updateDocument(
+            this.dbId,
+            this.usersCol,
+            String(telegramId),
+            {
+                puuid: tftData.puuid ?? null,
+                gameName: tftData.gameName ?? null,
+                tagLine: tftData.tagLine ?? null,
+                tier: tftData.tier ?? null,
+                rank: tftData.rank ?? null,
+                leaguePoints: tftData.leaguePoints ?? 0,
+                profileIconId: tftData.profileIconId ?? null
+            }
+        );
+    }
 
     _formatUser(user) {
         return {
-            userId: user.$id,
-            telegramId: user.telegramId
+            telegramId: user.$id,
+            puuid: user.puuid ?? null,
+            gameName: user.gameName ?? 'N/A',
+            rank: user.tier && user.rank 
+                ? `${user.tier} ${user.rank}` 
+                : 'Unranked'
         };
     }
 }
