5c4b43f0bb9c6964589038c44fc0bdc07b39422e
Mèo đi Mưa <tieunhichomuoicanruou@gmail.com>
2026-04-14 20:28:46 +0700

    Fix API key assignment in appwrite.config.js

--- src/config/appwrite.config.js
+++ src/config/appwrite.config.js
@@ -3,7 +3,7 @@ const { Client, Databases } = require("node-appwrite");
 const client = new Client()
     .setEndpoint(process.env.APPWRITE_ENDPOINT)
     .setProject(process.env.APPWRITE_PROJECT_ID)
-    .setKey("process.env.APPWRITE_API_KEY");
+    .setKey(process.env.APPWRITE_API_KEY);
 
 const databases = new Databases(client);
 
